44c74d1b9f598a648ea5264d54d7c17540b3ae2e
Pszczole <cezary932@gmail.com>
2024-01-14 11:43:37 +0100

    First initial commit.

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent {
+
+  todos: string[] = [];
+
+  addTodo():void {
+    console.log("Aktualna lista todo: ", this.todos)
+  }
+}
